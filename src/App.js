9b012b64aa28f1990958064efde5807ddff686a4
AtikRahman <atik321@gmail.com>
2020-02-27 18:27:03 +0200

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { timeDate } from "./utils/time-date.js";
+import "./App.css";
+import Image1 from "./image/morning.jpeg";
+import Image2 from "./image/noon.jpeg";
+import Image3 from "./image/night.jpeg";
+import Image4 from "./image/sunset.jpeg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setTime(), 1000);
+  }
+  setTime = () => {
+    this.setState({});
+  };
+  componentWillMo() {
+    clearInterval(this.interval);
+  }
+  weatherImage = () => {
+    let timeNow = new Date().getHours();
+    switch (true) {
+      case timeNow <= 12:
+        // return "Now it is morning";
+        return (
+          <div>
+            <h1 className="moment">Good Morning! {"Name"}</h1>
+            <img src={Image1} alt={"moring"} />;
+          </div>
+        );
+      case timeNow <= 16:
+        return (
+          <div>
+            <h1 className="moment"> Good Afternoon! {"Atik"}</h1>
+            <img src={Image2} alt={"Noon"} />;
+          </div>
+        );
+      case timeNow <= 18:
+        return (
+          <div>
+            <h1 className="moment"> Good Afternoon! {"Atik"}</h1>
+            <img src={Image4} alt={"Noon"} />;
+          </div>
+        );
+
+      case timeNow <= 24:
+        return (
+          <div>
+            <h1 className="moment">Good Night! {"Name"} </h1>
+            <img src={Image3} alt={"Night"} />;
+          </div>
+        );
+
+      default:
+        return "No one know about wather";
+    }
+  };
+  render() {
+    return (
+      <div className="main-container">
+        <h1 className="date"> {timeDate()}</h1>
+        <div className="weatherImageContainer">{this.weatherImage()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
